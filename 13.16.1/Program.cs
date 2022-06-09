cfe18735e590d18e871d350040d2d111774c6989
SkreimOF <2001main2001@gmail.com>
2022-06-09 17:56:56 +0200

    Добавьте файлы проекта.

---
+++ 13.16.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace Compare
+{
+    
+    class Program
+    {
+        static public string text = File.ReadAllText("D:\\Downloads\\test1.txt");
+        static void Main(string[] args)
+        {
+            // читаем весь файл с папки загрузки в строку текста
+           
+
+            Console.WriteLine("Method 1 Time Elapsed (ms): {0}", TimeMethod(List));
+            Console.WriteLine("Method 2 Time Elapsed (ms): {0}", TimeMethod(LinkedList));
+
+        }
+        static double TimeMethod(Action methodToTime)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            methodToTime();
+            stopwatch.Stop();
+            return stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        static void List()
+        {
+            var list = new List<string>();
+            list.Add(text);
+        }
+        
+        static void LinkedList()
+        {
+            var linkedlist = new LinkedList<string>();
+            linkedlist.AddFirst(text);
+        }
+    }
+}
